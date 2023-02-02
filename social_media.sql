72957843ebf1bda9ef33dc64da4071444862f086
SkyPH03 <81352625+SoraShiro03@users.noreply.github.com>
2023-02-02 20:33:23 +0630

    initial commit 2

--- social_media.sql
+++ social_media.sql
@@ -1,15 +1,15 @@
 -- created!
-create table UserAccount(
+create table User_Account(
 	UID varchar(50),
 	name varchar(50),
 	email varchar(50),
 	password varchar(50),
-	date_created date,
-	date_modified date,
+	date_created date default now(),
+	-- date_modified date default now(),
 	primary key(UID)
 );
-
-create table UserProfile(
+-- created!
+create table User_Profile(
 	UID varchar(50),
     user_name varchar(50),
     bio varchar(255),
@@ -20,22 +20,22 @@ create table UserProfile(
     -- user_save_collection int,
     date_created date,
     date_modified date,
-		FOREIGN KEY (UID) REFERENCES User(UID),
+		FOREIGN KEY (UID) REFERENCES User_Account(UID),
         FOREIGN KEY (user_hobbies) REFERENCES Hobbies(hobby_name)
         -- FOREIGN KEY (user_friends) REFERENCES Friends(user_profile_id),
         -- FOREIGN KEY (user_save_collection) REFERENCES SaveCollection(save_item),
 		-- FOREIGN KEY (UID) references SaveCollection(author)
 );
 
-create table Page(
+create table User_Page(
 	page_name varchar(50),
     page_profile_image varchar(155),
     page_cover_image varchar(155),
     -- followers int,
 	page_admin varchar(50),
     page_post int,
-    date_created date,
-    date_modified date,
+    date_created date default now(),
+    -- date_modified date,
 		primary key (page_name),
         foreign key (page_admin) references UserProfile(UID)
         -- foreign key (followers) references UserProfile(UID)
@@ -47,12 +47,12 @@ create table Hobbies(
     primary key(hobby_name)
 );
 
--- UserProfile need to create first
+-- created!
 create table Friends(
 	user_profile_id varchar(50),
     friend_profile_id varchar(50),
-    foreign key(user_profile_id) references UserProfile(UID),
-    foreign key(friend_profile_id) references UserProfile(UID)
+    foreign key(user_profile_id) references User_Profile(UID),
+    foreign key(friend_profile_id) references User_Profile(UID)
 );
 -- UserProfile need to create first
 create table Post(
@@ -60,30 +60,53 @@ create table Post(
     caption varchar(255),
     post_image varchar(255),
     -- like_count int,
-    date_created date,
+    date_created date default now(),
     author varchar(50),
-    primary key (post_id),
-    foreign key (author) references UserProfile(UID)
+		primary key (post_id),
+		foreign key (author) references User_Profile(UID)
 );
 -- UserProfile need to create first
-create table UserGroup(
-	group_name varchar(50),
+create table User_Group(
+	group_name varchar(255),
     group_admin varchar(50),
     group_cover_image varchar(155),
-    group_members int,
-    group_posts int,
-    date_created date,
-	primary key(group_name)
+    -- group_members int,
+    -- group_posts int,
+    date_created date default now(),
+		primary key(group_name),
+		foreign key(group_admin) references User_Profile(UID)
+);
+-- Group Post Table 
+create table Group_Post(
+	group_name varchar(255),
+    user_post_id varchar(255),
+		foreign key(group_name) references User_Group(group_name),
+        foreign key(user_post_id) references Post(post_id)
 );
 -- UserProfile need to create first
-create table SaveCollection(
+create table Save_Collection(
 	save_item int,
     author varchar(50), 
     foreign key(save_item) references Post(post_id),
     foreign key(author) references UserProfile(UID)
 );
 
+-- Reaction table for Post table
+create table Reaction(
+	user_post_id varchar(255),
+    user_profile_id varchar(50),
+    user_reaction varchar(20),
+		foreign key(user_post_id) references Post(post_id),
+        foreign key(user_profie_id) references UserProfile(UID),
+        foreign key(user_reaction) references Reaction_Type(reaction_name)
+);
+create table Reaction_Type(
+	reaction_name varchar(20),
+		primary key(reaction_name)
+);
+    
 show databases;
 use social_media;
 create database social_mediaDB;
 show tables;
+drop table useraccount;

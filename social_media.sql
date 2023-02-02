dcd46b11e08188e3ee2d75429a3bb13425a97160
SkyPH03 <81352625+SoraShiro03@users.noreply.github.com>
2023-02-02 21:38:55 +0630

    commit 3 social_media data adding and table created with no error!

--- social_media.sql
+++ social_media.sql
@@ -1,32 +1,80 @@
--- created!
+-- Main Table For all user to authenticate
 create table User_Account(
 	UID varchar(50),
 	name varchar(50),
 	email varchar(50),
 	password varchar(50),
-	date_created date default now(),
-	-- date_modified date default now(),
-	primary key(UID)
+    
+	date_created datetime default now(),
+	date_modified datetime on update now(),
+		primary key(UID)
 );
--- created!
+-- User Hobby category
+create table Hobbies(
+	hobby_name varchar(30),
+    primary key(hobby_name)
+);
+-- Main Table for all user to access other tables
 create table User_Profile(
 	UID varchar(50),
     user_name varchar(50),
     bio varchar(255),
+    birthday date,
     profile_image varchar(255),
     profile_cover_image varchar(255),
     user_hobbies varchar(50),
-    -- user_friends varchar(50),
-    -- user_save_collection int,
-    date_created date,
-    date_modified date,
+    
+    date_created datetime default now(),
+    date_modified datetime on update now(),
 		FOREIGN KEY (UID) REFERENCES User_Account(UID),
         FOREIGN KEY (user_hobbies) REFERENCES Hobbies(hobby_name)
-        -- FOREIGN KEY (user_friends) REFERENCES Friends(user_profile_id),
-        -- FOREIGN KEY (user_save_collection) REFERENCES SaveCollection(save_item),
-		-- FOREIGN KEY (UID) references SaveCollection(author)
 );
 
+-- Making connection with each other(users) like see your friend post like facebook
+create table User_Friends(
+	user_profile_id varchar(50),
+    friend_profile_id varchar(50),
+    foreign key(user_profile_id) references User_Profile(UID),
+    foreign key(friend_profile_id) references User_Profile(UID)
+);
+
+
+-- UserProfile need to create first
+create table Post(
+	post_id varchar(255),
+    caption varchar(255),
+    post_image varchar(255),
+    -- like_count int,
+    date_created datetime default now(),
+    author varchar(50),
+		primary key (post_id),
+		foreign key (author) references User_Profile(UID)
+);
+
+-- Reaction type for Reaction Table (like, love, care, haha, upvote, downvote...) <- reaction types
+create table Reaction_Type(
+	reaction_name varchar(20),
+		primary key(reaction_name)
+);
+-- Reaction table for Post table (user can give their reaction like in facebook when you give reaction at other user's post)
+create table Reaction(
+	user_post_id varchar(255),
+    user_profile_id varchar(50),
+    user_reaction varchar(20),
+		foreign key(user_post_id) references Post(post_id),
+        foreign key(user_profile_id) references User_Profile(UID),
+        foreign key(user_reaction) references Reaction_Type(reaction_name)
+);
+
+-- To save your post
+create table User_Save_Collection(
+	save_item varchar(255),
+    author varchar(50), 
+		foreign key(save_item) references Post(post_id),
+		foreign key(author) references User_Profile(UID)
+);
+
+-- This table for user to create his/her page (like in facebook)
 create table User_Page(
 	page_name varchar(50),
     page_profile_image varchar(155),
@@ -34,79 +82,56 @@ create table User_Page(
     -- followers int,
 	page_admin varchar(50),
     page_post int,
-    date_created date default now(),
-    -- date_modified date,
+    date_created datetime default now(),
+    date_modified datetime on update now(),
 		primary key (page_name),
-        foreign key (page_admin) references UserProfile(UID)
+        foreign key (page_admin) references User_Profile(UID)
         -- foreign key (followers) references UserProfile(UID)
         
 );
--- created!
-create table Hobbies(
-	hobby_name varchar(30),
-    primary key(hobby_name)
-);
-
--- created!
-create table Friends(
-	user_profile_id varchar(50),
-    friend_profile_id varchar(50),
-    foreign key(user_profile_id) references User_Profile(UID),
-    foreign key(friend_profile_id) references User_Profile(UID)
-);
--- UserProfile need to create first
-create table Post(
-	post_id varchar(255),
-    caption varchar(255),
-    post_image varchar(255),
-    -- like_count int,
-    date_created date default now(),
-    author varchar(50),
-		primary key (post_id),
-		foreign key (author) references User_Profile(UID)
-);
--- UserProfile need to create first
+-- Users can intereact each other more easily when users want to intereact multiple users at the same time or same place
 create table User_Group(
 	group_name varchar(255),
     group_admin varchar(50),
     group_cover_image varchar(155),
     -- group_members int,
     -- group_posts int,
-    date_created date default now(),
+    date_created datetime default now(),
 		primary key(group_name),
 		foreign key(group_admin) references User_Profile(UID)
 );
--- Group Post Table 
+-- Group Post collection table (so only group memebers can see it) 
 create table Group_Post(
 	group_name varchar(255),
     user_post_id varchar(255),
 		foreign key(group_name) references User_Group(group_name),
         foreign key(user_post_id) references Post(post_id)
 );
--- UserProfile need to create first
-create table Save_Collection(
-	save_item int,
-    author varchar(50), 
-    foreign key(save_item) references Post(post_id),
-    foreign key(author) references UserProfile(UID)
+-- For Group Members
+create table Group_Members(
+	group_name varchar(255),
+    user_profile_id varchar(50),
+		foreign key(group_name) references User_Group(group_name),
+        foreign key(user_profile_id) references User_Profile(UID)
 );
 
--- Reaction table for Post table
-create table Reaction(
-	user_post_id varchar(255),
-    user_profile_id varchar(50),
-    user_reaction varchar(20),
-		foreign key(user_post_id) references Post(post_id),
-        foreign key(user_profie_id) references UserProfile(UID),
-        foreign key(user_reaction) references Reaction_Type(reaction_name)
-);
-create table Reaction_Type(
-	reaction_name varchar(20),
-		primary key(reaction_name)
-);
-    
+
+-- My Table Creation Order!
+-- 1 User_Account
+-- 2 Hobbies
+-- 3 User_Profile
+-- 4 User_Friends
+-- 5 Post
+-- 6 Reaction
+-- 7 Reaction_Type
+-- 8 User_Save_Collection
+-- 9 User_Page
+-- 10 User_Group
+-- 11 Group_Post
+-- 12 Group_Members
+
 show databases;
 use social_media;
 create database social_mediaDB;
 show tables;
-drop table useraccount;
+drop table user_account;
---
+++ social_media_data.sql
@@ -0,0 +1,18 @@
+insert into user_account values('KPTMYK--001209','Lwin Oo Maung Maung', 'lwinoomaungmaung@gmail.com', 'lwin');
+ insert into user_account values('KPTMYK--001171','Aung Phyo Zaw', 'aungphyozaw@gmail.com', 'aung');
+ insert into user_account values('KPTMYK--001162','Thazin Moe', 'thazinmoe@gmail.com', 'thazin');
+ insert into user_account values('KPTMYK--001161','Hnin Yu Ya Hlaing', 'hninyuyahlaing@gmail.com', 'hnin');
+ insert into user_account values('KPTMYK--001165','Yin Myint Myat Thu', 'yinmyintmyatthu@gmail.com', 'thu');
+ insert into user_account values('KPTMYK--001087','Su Su Hlaing', 'susuhlaing@gmail.com', 'susu');
+ insert into user_account values('KPTMYK--001100','Zin Yoon Phyu', 'zinyoonphyu@gmail.com', 'phyu');
+ insert into user_account values('KPTMYK--001112','Soe Min Htet', 'soeminhtet@gmail.com', 'soe');
+ insert into user_account values('KPTMYK--001068','Mya Anaga San', 'myaanagasan@gmail.com', 'mya');
+ insert into user_account values('KPTMYK--001067','Hsu Nyeik Wai', 'hsunyeikwai@gmail.com', 'hsu');
+ insert into user_account values('KPTMYK--001227','Ye Min Oo', 'yeminoo@gmail.com', 'ye');
+ insert into user_account values('KPTMYK--001379','Soe Than', 'soethan@gmail.com', 'than');
+ insert into user_account values('KPTMYK--001219','Zue Lin Let Lwin', 'zuelinletlwin@gmail.com', 'zue');
+ insert into user_account values('KPTMYK--001217','Pyae Hein', 'pyaehein@gmail.com', 'pyae');
+ insert into user_account values('KPTMYK--001232','Hlyan Htet Aung', 'hlyanhtetaung@gmail.com', 'hlyan');
+ insert into user_account values('KPTMYK--001169','Min Pyae Sone', 'minpyaesone@gmail.com', 'min');
+ insert into user_account values('KPTMYK--001103','TheinKha Kyaw', 'theinkhakyaw@gmail.com', 'theinkha');
+ 
